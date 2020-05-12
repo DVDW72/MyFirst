a4d6a829322916bbd2366515ea9c57bff3bb814c
DVD <DVD@TLV-TCS-RES11.TELEVIC.com>
2020-05-12 21:00:24 +0200

    2nd version

--- MyFirst/src/main/java/com/example/demo/MyFirstApplication.java
+++ MyFirst/src/main/java/com/example/demo/MyFirstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyFirstApplication {
 
 	public static void main(String[] args) {
+		// added text 
 		SpringApplication.run(MyFirstApplication.class, args);
 	}
 
